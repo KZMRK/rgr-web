e215db8fb57ac136506c6398b9d49cc64a0dada9
vyllian <anastasiia.vylitkova.kn.2021@lpnu.ua>
2023-05-16 00:24:45 +0300

    made "become-volunteer" button work

--- scripts/volunteering.js
+++ scripts/volunteering.js
@@ -62,3 +62,123 @@ inputElement.addEventListener('keyup',formatToPhone);
 
 // form submit button
 
+var frame = document.getElementById('thank-frame');
+var submitButton = document.getElementById('become-volunteer');
+const nodeList= document.querySelectorAll("section");
+
+submitButton.addEventListener('click', function(event) {
+    event.preventDefault(); 
+    var name = document.getElementById('name-input')
+    if (name.value.replace(/(\s\s+)/g, ' ').length <2){
+        name.style.backgroundColor="#D44949";
+        return;
+    }
+    else name.style.background="none";
+
+    var number = document.getElementById('number-input');
+    if (number.value.replace(/(\s\s+)/g, ' ').length <22){
+        number.style.backgroundColor="#D44949";
+        return;
+    }
+    else number.style.background="none";
+
+    var help = document.getElementById('help-input');
+    if (help.value.replace(/(\s\s+)/g, ' ').length <2){
+        help.style.backgroundColor="#D44949";
+        return;
+    }
+    else help.style.background="none";
+
+    var checked = false;
+    var radioButtons = document.querySelectorAll('input[name="frequency"]');
+    for (const radioButton of radioButtons) {
+        if (radioButton.checked) {
+            checked=true;
+            break;
+        }
+    }
+    var ques = document.getElementById('frequency-question');
+    if(!checked){
+        ques.style.backgroundColor = "#D44949";
+        ques.style.border = "#D44949 3px solid"
+        ques.style.borderRadius="10px"
+        return;
+    }
+    else { ques.style.background="none";
+        ques.style.border ="none";
+    }
+    checked=false;
+
+    radioButtons = document.querySelectorAll('input[name="pet"]');
+    for (const radioButton of radioButtons) {
+        if (radioButton.checked) {
+            checked=true;
+            break;
+        }
+    }
+    ques = document.getElementById('pet-question');
+    if(!checked){
+        ques.style.backgroundColor = "#D44949";
+        ques.style.border = "#D44949 3px solid"
+        ques.style.borderRadius="10px"
+        return;
+    }
+    else { ques.style.background="none";
+        ques.style.border ="none";
+    }
+    checked=false;
+    
+    radioButtons = document.querySelectorAll('input[name="shelter"]');
+    for (const radioButton of radioButtons) {
+        if (radioButton.checked) {
+            checked=true;
+            break;
+        }
+    }
+     ques = document.getElementById('shelter-question');
+    if(!checked){
+        ques.style.backgroundColor = "#D44949";
+        ques.style.border = "#D44949 3px solid"
+        ques.style.borderRadius="10px"
+        return;
+    }
+    else { ques.style.background="none";
+        ques.style.border ="none";
+    }
+
+    document.getElementById('overlay').style.visibility="visible";
+    frame.style.visibility="visible";
+
+});
+
+function close_frame(){
+    document.getElementById('overlay').style.visibility="hidden";
+    frame.style.visibility="hidden";
+    phoneInput.value='';
+    phoneInput.placeholder='Номер телефону';
+    document.getElementById('name-input').value='';
+    document.getElementById('help-input').value='';
+   document.getElementById('which-input').value='';
+   
+    var radioButtons = document.querySelectorAll('input[name="frequency"]');
+    for (const radioButton of radioButtons) {
+        if (radioButton.checked) {
+            radioButton.checked=false;
+        }
+    }
+    
+    radioButtons = document.querySelectorAll('input[name="pet"]');
+    for (const radioButton of radioButtons) {
+        if (radioButton.checked) {
+            radioButton.checked=false;
+        }
+    }
+    
+    radioButtons = document.querySelectorAll('input[name="shelter"]');
+    for (const radioButton of radioButtons) {
+        if (radioButton.checked) {
+            radioButton.checked=false;
+        }
+    }
+
+}
