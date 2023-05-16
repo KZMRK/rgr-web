36d7e821bb376626256044535618c1de1b73c524
vyllian <anastasiia.vylitkova.kn.2021@lpnu.ua>
2023-05-16 23:49:10 +0300

    added big photo slider

--- scripts/volunteering.js
+++ scripts/volunteering.js
@@ -1,4 +1,30 @@
 //trustee photo slider
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function big_photo(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+ 
+  slides[slideIndex-1].style.display = "block";
+ 
+}
+
 
 
 // phone number validation
